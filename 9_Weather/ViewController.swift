bd938eec825caaeb17020fad437103bdba25938d
YSLee-Dev <ysleedev01@gmail.com>
2022-02-07 18:37:59 +0900

    프로젝트 생성

---
+++ 9_Weather/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  9_Weather
+//
+//  Created by 이윤수 on 2022/02/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        self.view.backgroundColor = .white
+    }
+}
+
